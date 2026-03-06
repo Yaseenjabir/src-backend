f7e56c4069d80fdd794b2be6a4e1c36e2986e372
yaseenjabir <yaseenkhan1235476@gmail.com>
2026-03-07 00:39:16 +0500

    change:Added some change to summary logic

--- src/routes/summary.routes.ts
+++ src/routes/summary.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getReceivablesSummary } from "../controllers/summary.controller.js";
+import {
+  getDashboardSummary,
+  getReceivablesSummary,
+} from "../controllers/summary.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 
 router.get("/receivables", asyncHandler(getReceivablesSummary));
+router.get("/dashboard", asyncHandler(getDashboardSummary));
 
 export default router;
